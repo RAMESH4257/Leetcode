a9a0376e6baa46786bd9305c00df483296593c25
RAMESH4257 <155468155+RAMESH4257@users.noreply.github.com>
2025-10-30 17:03:02 +0530

    Time: 2 ms (97.84%) | Memory: 45.3 MB (5.55%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.java
+++ 3226-minimum-number-game/minimum-number-game.java
@@ -15,18 +15,27 @@ class Solution {
     //     return arr;
 
     // 2nd method
-    Queue<Integer> pq=new PriorityQueue<>();
-    int arr[]=new int[nums.length];
-    for(int i=0;i<nums.length;i++){
-        pq.add(nums[i]);
+    // Queue<Integer> pq=new PriorityQueue<>();
+    // int arr[]=new int[nums.length];
+    // for(int i=0;i<nums.length;i++){
+    //     pq.add(nums[i]);
+    // }
+    // int idx=0;
+    // while(pq.size()>0){
+    //     int alice=pq.poll();
+    //     int bob=pq.poll();
+    //     arr[idx++]=bob;
+    //     arr[idx++]=alice;
+    // }
+    // return arr;  
+     
+    // 3rd method
+    Arrays.sort(nums);
+    for(int i=0;i<nums.length;i+=2){
+        int temp=nums[i];
+        nums[i]=nums[i+1];
+        nums[i+1]=temp;
     }
-    int idx=0;
-    while(pq.size()>0){
-        int alice=pq.poll();
-        int bob=pq.poll();
-        arr[idx++]=bob;
-        arr[idx++]=alice;
-    }
-    return arr;
+    return nums;
     }
 }
