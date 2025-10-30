9f467190e4dc7d0afbf3e98131b305aec9ec77a7
RAMESH4257 <155468155+RAMESH4257@users.noreply.github.com>
2025-10-30 17:00:44 +0530

    Time: 3 ms (32.20%) | Memory: 44.8 MB (79.03%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.java
+++ 3226-minimum-number-game/minimum-number-game.java
@@ -1,16 +1,32 @@
 class Solution {
     public int[] numberGame(int[] nums) {
-       int idx=0;
-        Arrays.sort(nums);
-        int arr[]=new int[nums.length];
-        //int idx=0;
-        for(int i=0;i< nums.length-1;i++){
-            int a=nums[i];
-            int b=nums[i+1];
-            i+=1;
-            arr[idx++]=b;
-            arr[idx++]=a;
-        }
-        return arr;
+        // 1st method
+    //    int idx=0;
+    //     Arrays.sort(nums);
+    //     int arr[]=new int[nums.length];
+    //     //int idx=0;
+    //     for(int i=0;i< nums.length-1;i++){
+    //         int a=nums[i];
+    //         int b=nums[i+1];
+    //         i+=1;
+    //         arr[idx++]=b;
+    //         arr[idx++]=a;
+    //     }
+    //     return arr;
+
+    // 2nd method
+    Queue<Integer> pq=new PriorityQueue<>();
+    int arr[]=new int[nums.length];
+    for(int i=0;i<nums.length;i++){
+        pq.add(nums[i]);
+    }
+    int idx=0;
+    while(pq.size()>0){
+        int alice=pq.poll();
+        int bob=pq.poll();
+        arr[idx++]=bob;
+        arr[idx++]=alice;
+    }
+    return arr;
     }
 }
