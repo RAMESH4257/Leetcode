1bc2043222ad5af98933017368c07473c18ba187
RAMESH4257 <155468155+RAMESH4257@users.noreply.github.com>
2025-12-01 19:36:03 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (26.00%) - LeetSync

--- 2491-smallest-even-multiple/smallest-even-multiple.java
+++ 2491-smallest-even-multiple/smallest-even-multiple.java
@@ -1,11 +1,17 @@
 class Solution {
     public int smallestEvenMultiple(int n) {
-        int min=Integer.MAX_VALUE;
-        for(int i=1;i<=5;i++){
-            int k=n*i;
-            if(k%2==0){
-               min=Math.min(k,min);
-            }
+        // int min=Integer.MAX_VALUE;
+        // for(int i=1;i<=5;i++){
+        //     int k=n*i;
+        //     if(k%2==0){
+        //        min=Math.min(k,min);
+        //     }
+        // }
+        int min=0;
+        if(n%2==0){
+            min=n;
+        }else{
+            min=n*2;
         }
         return min;
     }
