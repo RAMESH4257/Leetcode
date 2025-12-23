5cc7d3371231805027a87b269de0fdc3bc464097
RAMESH4257 <155468155+RAMESH4257@users.noreply.github.com>
2025-12-23 20:01:58 +0530

    Time: 6 ms (82.45%) | Memory: 45.9 MB (18.24%) - LeetSync

---
+++ 830-largest-triangle-area/largest-triangle-area.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    private double area(int[] a, int[] b, int[] c) {
+        return Math.abs(
+                a[0] * (b[1] - c[1]) +
+                b[0] * (c[1] - a[1]) +
+                c[0] * (a[1] - b[1])
+        ) / 2.0;
+    }
+
+    public double largestTriangleArea(int[][] points) {
+
+        double maxArea = 0.0;
+        int n = points.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    maxArea = Math.max(
+                        maxArea,
+                        area(points[i], points[j], points[k])
+                    );
+                }
+            }
+        }
+
+        return maxArea;
+    }
+}
