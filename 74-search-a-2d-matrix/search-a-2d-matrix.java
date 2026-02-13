961c010b68081410c971d979480d7c77762a17a1
RAMESH4257 <155468155+RAMESH4257@users.noreply.github.com>
2026-02-13 20:52:59 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (40.88%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,12 +1,37 @@
 class Solution {
+     static int binarySearch(int[] arr,int tar){
+        int l=0;
+        int h=arr.length-1;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(arr[mid]==tar){
+                return  mid;
+            }else if(arr[mid] >tar){
+                h=mid-1;
+            }else{
+                l=mid+1;
+            }
+        }
+        return -1;
+    }
     public boolean searchMatrix(int[][] matrix, int target) {
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[i].length;j++){
-                if(matrix[i][j]==target){
-                    return true;
-                }
+        int row=matrix.length;
+        int col=matrix[0].length;
+        int rowlow=0;
+        int rowhigh=row-1;
+        while(rowlow<=rowhigh){
+            int rowmid=rowlow+(rowhigh-rowlow)/2;
+            if(target>=matrix[rowmid][0] && target<=matrix[rowmid][col-1]){
+                int index=binarySearch(matrix[rowmid],target);
+                return index>=0;
+                    //return
+                
+            } else if (matrix[rowmid][0]>target) {
+                rowhigh=rowmid-1;
+            }else{
+                rowlow=rowmid+1;
             }
         }
         return false;
     }
-}
+    }
