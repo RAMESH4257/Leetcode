86bd3af7889a26f7d3a61f2a00010912f36c060a
RAMESH4257 <155468155+RAMESH4257@users.noreply.github.com>
2026-02-28 11:52:29 +0530

    Time: 5 ms (2.87%) | Memory: 44.8 MB (12.99%) - LeetSync

--- 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.java
+++ 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.java
@@ -1,17 +1,36 @@
 class Solution {
     public int countPairs(List<Integer> nums, int target) {
-        int arr[]=new int[nums.size()];
+        // int arr[]=new int[nums.size()];
+        // for(int i=0;i<nums.size();i++){
+        //     arr[i]=nums.get(i);
+        // }
+        // int c=0;
+        // for(int i=0;i<arr.length-1;i++){
+        //     for(int j=i+1;j<arr.length;j++){
+        //         if(arr[i]+arr[j]<target){
+        //             c++;
+        //         }
+        //     }
+        // }
+        // OPTIMAL-TWO POINTERS
+        int[] arr=new int[nums.size()];
         for(int i=0;i<nums.size();i++){
             arr[i]=nums.get(i);
         }
+        Arrays.sort(arr);
+        int l=0;
+        int r=arr.length-1;
         int c=0;
-        for(int i=0;i<arr.length-1;i++){
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i]+arr[j]<target){
-                    c++;
-                }
+        while(l<r){
+            int sum=arr[l]+arr[r];
+            if(sum<target){
+                c+=r-l;
+                l++;
+            }else {
+                r--;
             }
         }
+        
         return c;
 
     }
