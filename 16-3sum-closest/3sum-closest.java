ef2efdfb62b7a34ee1c6b20342d60dddf4ac70ad
RAMESH4257 <155468155+RAMESH4257@users.noreply.github.com>
2026-03-10 10:38:14 +0530

    Time: 12 ms (99.21%) | Memory: 45.5 MB (80.21%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -2,25 +2,47 @@ class Solution {
     public int threeSumClosest(int[] nums, int target) {
         int closetsum=0;
         int diff=Integer.MAX_VALUE;
+        Arrays.sort(nums);
         for(int i=0;i<nums.length-2;i++){
-            for(int j=i+1;j<nums.length-1;j++){
-               for(int k=j+1;k<nums.length;k++){
-                 int sum=nums[i]+nums[j]+nums[k];
-                 if(sum>target){
-                    if(diff > sum-target){
+            // for(int j=i+1;j<nums.length-1;j++){
+            //    for(int k=j+1;k<nums.length;k++){
+            //      int sum=nums[i]+nums[j]+nums[k];
+            //      if(sum>target){
+            //         if(diff > sum-target){
+            //             diff=sum-target;
+            //             closetsum=sum;
+            //         }
+            //      }else if(sum<target){
+            //         if(diff>target-sum){
+            //             diff=(target-sum);
+            //             closetsum=sum;
+            //         }
+            //      }
+            //      else{
+            //         return target;
+            //      }
+            //    }
+            // }
+            // two pointers
+             int j=i+1;
+            int k=nums.length-1;
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum>target){
+                    k--;
+                    if(diff>sum-target){
                         diff=sum-target;
                         closetsum=sum;
                     }
-                 }else if(sum<target){
+                }else if(sum<target){
+                    j++;
                     if(diff>target-sum){
-                        diff=(target-sum);
+                        diff=target-sum;
                         closetsum=sum;
                     }
-                 }
-                 else{
+                }else{
                     return target;
-                 }
-               }
+                }
             }
         }
         return closetsum;
