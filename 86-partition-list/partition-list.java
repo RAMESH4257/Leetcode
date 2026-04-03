6d3838e328825cb4ea7836de69616bdf851a82cd
RAMESH4257 <155468155+RAMESH4257@users.noreply.github.com>
2026-04-03 08:54:36 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (74.62%) - LeetSync

--- 86-partition-list/partition-list.java
+++ 86-partition-list/partition-list.java
@@ -10,38 +10,56 @@
  */
 class Solution {
     public ListNode partition(ListNode head, int x) {
-        ListNode temp=head;
-        ListNode dummy=new ListNode(0);
-        ListNode curr=dummy;
-        int n=0;
-        while(temp!=null){
-            n++;
-            temp=temp.next;
-        }
-        int[] ans=new int[n];
-        temp=head;
-        int idx=0;
-        while(temp!=null){
-            if(temp.val<x){
-                ans[idx++]=temp.val;
-                //temp=temp.next;
-            }
-            temp=temp.next;
-        }
-        temp=head;
-        while(temp!=null){
-            if(temp.val>=x){
-                ans[idx++]=temp.val;
-                //temp=temp.next;
-            }
-            temp=temp.next;
-        }
+        // ListNode temp=head;
+        // ListNode dummy=new ListNode(0);
+        // ListNode curr=dummy;
+        // int n=0;
+        // while(temp!=null){
+        //     n++;
+        //     temp=temp.next;
+        // }
+        // int[] ans=new int[n];
+        // temp=head;
+        // int idx=0;
+        // while(temp!=null){
+        //     if(temp.val<x){
+        //         ans[idx++]=temp.val;
+        //         //temp=temp.next;
+        //     }
+        //     temp=temp.next;
+        // }
+        // temp=head;
+        // while(temp!=null){
+        //     if(temp.val>=x){
+        //         ans[idx++]=temp.val;
+        //         //temp=temp.next;
+        //     }
+        //     temp=temp.next;
+        // }
     
-        for(int i=0;i<ans.length;i++){
-            ListNode newnode=new ListNode(ans[i]);
-            curr.next=newnode;
-            curr=newnode;
+        // for(int i=0;i<ans.length;i++){
+        //     ListNode newnode=new ListNode(ans[i]);
+        //     curr.next=newnode;
+        //     curr=newnode;
+        // }
+        // return dummy.next;
+    // optimal using 2 pointers without extra space 
+     ListNode smalldummy=new ListNode(0);
+        ListNode bigdummy=new ListNode(0);
+        ListNode small=smalldummy;
+        ListNode big=bigdummy;
+        while(head!=null){
+            if(head.val<x){
+                small.next=head;
+                small=head;
+            }else{
+                big.next=head;
+                big=head;
+            }
+            head=head.next;
         }
-        return dummy.next;
+        big.next=null;
+        small.next=bigdummy.next;
+        return smalldummy.next;
     }
 }
