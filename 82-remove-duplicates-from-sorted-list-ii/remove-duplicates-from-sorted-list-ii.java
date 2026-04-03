293d68dd8478fb429f01a3b838855d4af89363d8
RAMESH4257 <155468155+RAMESH4257@users.noreply.github.com>
2026-04-03 08:09:36 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (63.17%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.java
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.java
@@ -10,22 +10,60 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        LinkedHashMap<Integer,Integer> hm=new LinkedHashMap<>();
-        ListNode temp=head;
-        while(temp!=null){
-            int k=temp.val;
-            hm.put(k,hm.getOrDefault(k,0)+1);
-           temp=temp.next;
+        // LinkedHashMap<Integer,Integer> hm=new LinkedHashMap<>();
+        // ListNode temp=head;
+        // while(temp!=null){
+        //     int k=temp.val;
+        //     hm.put(k,hm.getOrDefault(k,0)+1);
+        //    temp=temp.next;
+        // }
+        // ListNode curr=new ListNode(0);
+        // ListNode dummy=curr;
+        // for(int i:hm.keySet()){
+        //     if(hm.get(i)==1){
+        //         ListNode newnode=new ListNode(i);
+        //         dummy.next=newnode;
+        //         dummy=newnode;
+        //     }
+        // }
+        // return curr.next;
+    // use 2 pointers time complexity is O(n) and space is O(1)
+        if(head==null){
+            return head;
         }
+        if(head.next==null){
+            return head;
+        }
+        if(head.val==head.next.val && head.next.next==null){
+            return null;
+        }
+        ListNode temp1=head;
+        ListNode temp2=null;
         ListNode curr=new ListNode(0);
         ListNode dummy=curr;
-        for(int i:hm.keySet()){
-            if(hm.get(i)==1){
-                ListNode newnode=new ListNode(i);
-                dummy.next=newnode;
-                dummy=newnode;
+        //boolean isvalid=true;
+        while(temp1!=null){
+            temp2=temp1.next;
+            boolean isvalid=false;
+            //nextnode=temp.next;
+            while(temp2!=null && temp1.val==temp2.val){
+                temp2=temp2.next;
+                isvalid=true;
             }
+            if(isvalid){
+                curr.next=null;
+                temp1=temp2;
+                // if(temp1==temp2){
+                //     break;
+                // }
+            }else{
+                curr.next=temp1;
+                curr=temp1;
+               temp1=temp2;
+            }
+            // temp1=temp2;
+            
         }
-        return curr.next;
+        return dummy.next;
     }
 }
