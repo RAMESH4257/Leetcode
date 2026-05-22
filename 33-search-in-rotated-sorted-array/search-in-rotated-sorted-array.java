3fef209e0f82ac3f0ee882471334ee073b898ae5
RAMESH4257 <155468155+RAMESH4257@users.noreply.github.com>
2026-05-22 13:02:28 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (45.24%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -31,3 +31,7 @@ class Solution {
      return -1;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
