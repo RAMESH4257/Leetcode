1bfe5cfbc808231af2b172520785aa7c47f17059
RAMESH4257 <155468155+RAMESH4257@users.noreply.github.com>
2026-05-27 19:10:34 +0530

    Time: 21 ms (66.94%) | Memory: 48.1 MB (64.88%) - LeetSync

---
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int numberOfSpecialChars(String word) {
+       int[] lastlower=new int[26];
+       int[] firstupper=new int[26];
+       Arrays.fill(lastlower,-1);
+       Arrays.fill(firstupper,-1);
+       for(int i=0;i<word.length();i++){
+        char ch=word.charAt(i);
+        if(Character.isLowerCase(ch)){
+            lastlower[ch-'a']=i;
+        }else{
+            int idx=ch-'A';
+            if(firstupper[idx]==-1){
+                firstupper[idx]=i;
+            }
+        }
+       }
+       int c=0;
+       for(int i=0;i<26;i++){
+        if(lastlower[i]!=-1 && firstupper[i]!=-1 && lastlower[i]<firstupper[i]){
+            c++;
+        }
+       }
+       return c;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
