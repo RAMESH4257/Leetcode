bed4d29f0584738fafd19dbfe2a6e07244e998c5
RAMESH4257 <155468155+RAMESH4257@users.noreply.github.com>
2026-05-29 13:07:46 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (31.30%) - LeetSync

---
+++ 31-next-permutation/next-permutation.java
@@ -0,0 +1,38 @@
+class Solution {
+    public void reverse(int[] nums,int l,int r){
+        while(l<r){
+            int temp=nums[l];
+            nums[l]=nums[r];
+            nums[r]=temp;
+            l++;
+            r--;
+        }
+    }
+    public void nextPermutation(int[] nums) {
+        int n=nums.length;
+        int ind=-1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+               ind=i;
+               break;
+            }
+        }
+        if(ind==-1){
+            reverse(nums,0,n-1);
+            return;
+        }
+        for(int i=n-1;i>ind;i--){
+            if(nums[i]>nums[ind]){
+                int temp=nums[i];
+                nums[i]=nums[ind];
+                nums[ind]=temp;
+                break;
+            }
+        }
+        reverse(nums,ind+1,n-1);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
