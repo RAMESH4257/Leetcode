aa17112b591a74dd23f4fba76b4d66a751fd961c
RAMESH4257 <155468155+RAMESH4257@users.noreply.github.com>
2026-06-08 20:35:47 +0530

    Time: 6 ms (6.17%) | Memory: 49.8 MB (12.38%) - LeetSync

---
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        if(root==null) return 0;
+        Queue<TreeNode> qu=new LinkedList<>();
+        qu.add(root);
+        int c=1;
+        while(!qu.isEmpty()){
+            TreeNode curr=qu.remove();
+            if(curr.left!=null){
+                c++;
+                qu.add(curr.left);
+            }
+            if(curr.right!=null){
+                c++;
+                qu.add(curr.right);
+            }
+        }
+        return c;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
