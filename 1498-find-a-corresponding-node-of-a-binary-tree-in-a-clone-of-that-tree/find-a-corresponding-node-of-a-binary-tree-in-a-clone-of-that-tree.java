e5477cfa3205e57bc47e00b75d398956d9c309a2
RAMESH4257 <155468155+RAMESH4257@users.noreply.github.com>
2026-06-14 20:23:48 +0530

    Time: 1 ms (100.00%) | Memory: 53.1 MB (82.65%) - LeetSync

---
+++ 1498-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+         if (original == null) {
+            return null;
+        }
+
+        if (original == target) {
+            return cloned;
+        }
+
+        TreeNode left = getTargetCopy(
+                original.left,
+                cloned.left,
+                target);
+
+        if (left != null) {
+            return left;
+        }
+
+        return getTargetCopy(
+                original.right,
+                cloned.right,
+                target);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
