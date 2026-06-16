eb31a5ab49a49670a37bd13cbf3641be0bc7f1ea
RAMESH4257 <155468155+RAMESH4257@users.noreply.github.com>
2026-06-16 11:19:14 +0530

    Time: 1 ms (9.56%) | Memory: 43.8 MB (15.27%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -14,19 +14,38 @@
  * }
  */
 class Solution {
-    public boolean check(TreeNode root1,TreeNode root2){
-        if(root1==null && root2==null){
-            return true;
-        }
-        if(root1==null||root2==null){
+    // public boolean check(TreeNode root1,TreeNode root2){
+    //     if(root1==null && root2==null){
+    //         return true;
+    //     }
+    //     if(root1==null||root2==null){
+    //         return false;
+    //     }
+    //     if(root1.val!=root2.val) return false;
+    //     return check(root1.left,root2.right)&&check(root1.right,root2.left);
+    // }
+    public boolean isSymmetric(TreeNode root) {
+        // if(root==null) return false;
+        // return check(root.left,root.right);
+        // iterative approach(level-order)
+        if(root==null){
             return false;
         }
-        if(root1.val!=root2.val) return false;
-        return check(root1.left,root2.right)&&check(root1.right,root2.left);
-    }
-    public boolean isSymmetric(TreeNode root) {
-        if(root==null) return false;
-        return check(root.left,root.right);
+        Queue<TreeNode> q=new LinkedList<>();
+        q.add(root.left);
+        q.add(root.right);
+        while(!q.isEmpty()){
+            TreeNode n1=q.remove();
+            TreeNode n2=q.remove();
+            if(n1==null && n2==null) continue;
+            if(n1==null || n2==null) return false;
+            if(n1.val!=n2.val) return false;
+            q.add(n1.left);
+            q.add(n2.right);
+            q.add(n1.right);
+            q.add(n2.left);
+        }
+        return true;
     }
 }
 
