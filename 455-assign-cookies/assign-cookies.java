02bbe74745f57d2544c2236b6ba21bd4983b8fd6
RAMESH4257 <155468155+RAMESH4257@users.noreply.github.com>
2026-06-27 12:18:14 +0530

    Time: 14 ms (93.15%) | Memory: 51.5 MB (55.75%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i=0,j=0;
+        while(i<s.length && j<g.length){
+            if(s[i]>=g[j]){
+                j++;
+            }
+            i++;
+        }
+        return j;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
