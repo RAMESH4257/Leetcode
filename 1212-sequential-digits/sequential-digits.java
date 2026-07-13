ec349569c0971b001b3ef49158849db99e93617e
Pandiri Ramesh <155468155+RAMESH4257@users.noreply.github.com>
2026-07-13 18:56:47 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (87.35%) - LeetSync

---
+++ 1212-sequential-digits/sequential-digits.java
@@ -0,0 +1,56 @@
+class Solution {
+    public int find(int[] arr,int x){
+        int low=0,high=arr.length-1;
+        int ans=-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]>=x){
+              ans=mid;
+              high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+    public int find2(int[] arr,int x){
+        int ans=-1;
+        int low=0,high=arr.length-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]<=x){
+                ans=mid;
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+    public List<Integer> sequentialDigits(int low, int high) {
+        List<Integer> li=new ArrayList<>();
+        int[] sequential = {
+    12, 23, 34, 45, 56, 67, 78, 89,
+    123, 234, 345, 456, 567, 678, 789,
+    1234, 2345, 3456, 4567, 5678, 6789,
+    12345, 23456, 34567, 45678, 56789,
+    123456, 234567, 345678, 456789,
+    1234567, 2345678, 3456789,
+    12345678, 23456789,
+    123456789
+};
+    int s=find(sequential,low);
+    int d=find2(sequential,high);
+    if (s == -1 || d == -1 || s > d) {
+       return li;
+}
+    for(int i=s;i<=d;i++){
+        li.add(sequential[i]);
+    }
+    return li;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
