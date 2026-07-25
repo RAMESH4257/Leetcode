064f69286b3c8d101c28f3136ed9e5f1aa90f3e6
Pandiri Ramesh <155468155+RAMESH4257@users.noreply.github.com>
2026-07-25 20:56:48 +0530

    Time: 6 ms (1.07%) | Memory: 43 MB (6.07%) - LeetSync

---
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        String a=String.format("%31s",Integer.toBinaryString(start)).replace(' ','0');
+        String b=String.format("%31s",Integer.toBinaryString(goal)).replace(' ','0');
+        int c=0;
+        for(int i=0;i<a.length();i++){
+            if(a.charAt(i)!=b.charAt(i)){
+                c++;
+            }
+        }
+        return c;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
