eb105a03956afc33a860a4388a4dcb28afeace21
Pandiri Ramesh <155468155+RAMESH4257@users.noreply.github.com>
2026-07-25 17:06:55 +0530

    Time: 2 ms (29.84%) | Memory: 43.2 MB (5.71%) - LeetSync

--- 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.java
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.java
@@ -1,12 +1,21 @@
 class Solution {
     public int maxProduct(int n) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
+       while(n!=0){
+        int r=n%10;
+        pq.add(r);
+        n=n/10;
+       }
+       int a=pq.poll();
+       int b=pq.poll();
+       return a*b;
         
-        String s=String.valueOf(n);
-        char[] arr=s.toCharArray();
-        Arrays.sort(arr);
-        int a=arr[arr.length-1]-'0';
-        int b=arr[arr.length-2]-'0';
-        return a*b;
+        // String s=String.valueOf(n);
+        // char[] arr=s.toCharArray();
+        // Arrays.sort(arr);
+        // int a=arr[arr.length-1]-'0';
+        // int b=arr[arr.length-2]-'0';
+        // return a*b;
 
     }
 }
