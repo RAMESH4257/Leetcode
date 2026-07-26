875b85eff04644b553ebb34bcd0514a8d66774f4
Pandiri Ramesh <155468155+RAMESH4257@users.noreply.github.com>
2026-07-26 15:45:52 +0530

    Time: 8 ms (12.91%) | Memory: 45.5 MB (41.18%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -1,19 +1,25 @@
 class Solution {
     public int singleNumber(int[] nums) {
         // use hashmap -brute force;
-        int ans=0;
-        for(int bit=0;bit<32;bit++){
-            int c=0;
-            for(int i=0;i<nums.length;i++){
-               if((nums[i]&(1<<bit))!=0){
-                c++;
-               }
-            }
-            if(c%3!=0){
-                ans=ans|(1<<bit);
-            }
+
+        // int ans=0;
+        // for(int bit=0;bit<32;bit++){
+        //     int c=0;
+        //     for(int i=0;i<nums.length;i++){
+        //        if((nums[i]&(1<<bit))!=0){
+        //         c++;
+        //        }
+        //     }
+        //     if(c%3!=0){
+        //         ans=ans|(1<<bit);
+        //     }
+        // }
+        // return ans; 
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length;i=i+3){
+            if(nums[i]!=nums[i-1]) return nums[i-1];
         }
-        return ans;
+        return nums[nums.length-1];
     }
 }
 
