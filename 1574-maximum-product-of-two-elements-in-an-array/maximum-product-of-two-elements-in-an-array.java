03b1a6dec5ab70d2fc03d98d11e36d2910646e35
Pandiri Ramesh <155468155+RAMESH4257@users.noreply.github.com>
2026-07-27 20:46:29 +0530

    Time: 5 ms (42.28%) | Memory: 45.3 MB (7.01%) - LeetSync

--- 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.java
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.java
@@ -1,11 +1,18 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        Arrays.sort(nums);
-        int prod=0,max=0;
-        for(int i=0;i<nums.length-1;i++){
-            prod=((nums[i]-1)*(nums[i+1]-1));
-            max=Math.max(max,prod);
-        }
-        return max;
+         Arrays.sort(nums);
+        int n=nums.length;
+        return (nums[n-1]-1)*(nums[n-2]-1);
+        //Arrays.sort(nums);
+        // int prod=0,max=0;
+        // for(int i=0;i<nums.length-1;i++){
+        //     prod=((nums[i]-1)*(nums[i+1]-1));
+        //     max=Math.max(max,prod);
+        // }
+        // return max;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
